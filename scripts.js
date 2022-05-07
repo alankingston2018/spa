72551900bcfa7ac84b9a3d4593ccd2f0c2a2f765
Hoàng Anh <anhphung@carp.vn>
2022-05-07 17:24:26 +0700

    update css

--- scripts.js
+++ scripts.js
@@ -1,4 +1,48 @@
-﻿var screen_width1 = jQuery(window).width();
+﻿////document.addEventListener('DOMContentLoaded', function () {
+////    var buttonScollToTop = document.querySelector('.scroll-to-top');
+
+////    window.addEventListener("scroll", function () {
+////        //Show button scroll to top
+////        if (window.pageYOffset > 300) {
+////            buttonScollToTop.classList.add("show");
+////        } else {
+////            buttonScollToTop.classList.remove("show")
+////        }
+////    })
+////    buttonScollToTop.addEventListener("click", function () {
+////        document.body.scrollTop = 0;
+////        document.documentElement.scrollTop = 0;
+////    })
+////});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    var t = document.querySelector(".vanillatop");
+    function e(n) {
+        var o = window.pageYOffset,
+            a = Math.floor(Date.now());
+        !(function t() {
+            Math.easeInOutQuad = function (t) {
+                return t < 0.5 ? 2 * t * t : (4 - 2 * t) * t - 1;
+            };
+            var e = Math.min(1, (Math.floor(Date.now()) - a) / n);
+            window.scroll(0, Math.ceil(Math.easeInOutQuad(e) * (0 - o) + o)), 0 === window.pageYOffset && callback(), requestAnimationFrame(t);
+        })();
+    }
+    t.addEventListener("click", function () {
+        window.requestAnimationFrame ? e(700) : window.scrollTo(0, 0);
+    }),
+        window.addEventListener("scroll", function () {
+            280 < document.body.scrollTop || 280 < document.documentElement.scrollTop
+                ? (t.removeAttribute("style", "transform: translateX(120px);"), t.setAttribute("style", "transform: translateX(0);"))
+                : (t.removeAttribute("style", "transform: translateX(0);"), t.setAttribute("style", "transform: translateX(120px);"));
+        });
+});
+
+
+
+
+var screen_width1 = jQuery(window).width();
 jQuery("#click-menu").on("click", function (t) {
     jQuery("#menu-toggle").toggleClass("active"), jQuery(this).toggleClass("active");
 }),
